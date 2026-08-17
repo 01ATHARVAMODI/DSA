0f3b7a925995bf2b81443ffae0bd01372554fe3b
AtharvaModi <atharvamodi06@gmail.com>
2026-08-17 23:54:12 +0530

    Time: 0 ms (100%), Space: 43.7 MB (19.5%) - LeetHub

---
+++ 1450-number-of-students-doing-homework-at-a-given-time/1450-number-of-students-doing-homework-at-a-given-time.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int busyStudent(int[] s, int[] e, int q) {
+
+        int c = 0;
+
+        for (int i = 0; i < s.length; i++) {
+            if (s[i] <= q && q <= e[i])
+                c++;
+        }
+
+        return c;
+    }
+}
